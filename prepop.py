988bbb60fb45d11f031faaa84fa3f8b100052aa0
Sana Javed <sanajaved7@gmail.com>
2015-02-08 00:42:38 -0500

    Started form to select Twitter or Facebook

--- prepop.py
+++ prepop.py
@@ -1,13 +1,11 @@
-def prepop_twitter(content):
-    if len(oldtweet) > 140:
-        print "Heads up! That tweet is more than 140 characters!"
+def prepop_twitter(oldtweet):
     new_tweet = oldtweet.replace(" ", "+").replace("#", "%23")
     header = "http://twitter.com/intent/tweet?text="
     return header + new_tweet
     # print "Here is your prepopulated Tweet! " + header + new_tweet
 
 
-def prepop_facebook(content):
+def prepop_facebook(oldlink):
     newlink = oldlink.replace("/", "%2F")
     fbshare = 'http://www.facebook.com/sharer.php?u=newlink'
     newfb = fbshare.replace("newlink", newlink)
@@ -21,6 +19,8 @@ if __name__ == "__main__":
 
         if content.lower() == "twitter":
             oldtweet = raw_input("Great. Enter the tweet you would like prepopulated: ")
+            if len(content) > 140:
+                print "Heads up! That tweet is more than 140 characters!"
             prepop_twitter(oldtweet)
 
         else:
--- prepopapp.py
+++ prepopapp.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import prepop
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    content = "This is my very first tweet of my life #wow"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
