e34c0aef3fa07ff76caac76d7a9f79c2c88d3906
Sana Javed <sanajaved7@gmail.com>
2015-02-08 21:28:24 -0500

    Basic structure of app done and working

--- prepopapp.py
+++ prepopapp.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import prepop
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
-    content = "This is my very first tweet of my life #wow"
-    return render_template("index.html")
+    if request.form:
+        if request.form['socialnetwork'] == "Twitter":
+            p = prepop.prepop_twitter(request.form['content'])
+        else:
+            p = prepop.prepop_facebook(request.form['content'])
+        return render_template("index.html",content=p)
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
