3b1ac4e1f90d6b652ef24008c22f9f2ae4858ef0
Sana Javed <sanajaved7@gmail.com>
2015-02-09 00:04:10 -0500

    Added Twitter Bootstrap and some styling

--- prepopapp.py
+++ prepopapp.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, request
+from flask.ext.bootstrap import Bootstrap
 import prepop
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/", methods=['GET','POST'])
 def index():
