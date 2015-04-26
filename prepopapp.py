621e9d3fe7336bfaa771f4955d56b031660dddd1
Sana Javed <sanajaved7@gmail.com>
2015-04-25 22:44:30 -0400

    Made some PEP8 changes

--- prepopapp.py
+++ prepopapp.py
@@ -5,14 +5,15 @@ import prepop
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-@app.route("/", methods=['GET','POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     if request.form:
         if request.form['socialnetwork'] == "Twitter":
             p = prepop.prepop_twitter(request.form['content'])
         else:
             p = prepop.prepop_facebook(request.form['content'])
-        return render_template("index.html",content=p)
+        return render_template("index.html", content=p)
     else:
         return render_template("index.html")
 
